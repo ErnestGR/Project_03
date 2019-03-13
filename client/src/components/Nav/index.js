8706cfcbef91ccc3fa10640bdb2617a22f9341c0
Netcore89 <ernes.rave@gmail.com>
2019-03-12 20:12:50 -0600

    chg: fixed typo

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <h1 className="navbar-brand">Avinco</h1>
+      <h1 className="navbar-brand">Abinco</h1>
       <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
         <li className="nav-item active">
           <Link className="nav-link" to="/">
