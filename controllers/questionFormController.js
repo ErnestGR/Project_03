e8b2a1db7e6af640d3260c75372a4b62b39ac663
gabrieldiaz23 <pacho.rudo22@gmail.com>
2019-03-21 19:16:54 -0600

    ya jaÃla. gracias chuy

--- controllers/questionFormController.js
+++ controllers/questionFormController.js
@@ -1,6 +1,13 @@
 const db = require('../models');
 
 module.exports = {
+  findAll: function (req, res) {
+    db.Question
+      .find(req.query)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function (req, res) {res.send(200)},
   create: function(req, res) {
     db.Question
       .create(req.body)
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const exampleRoutes = require("./example");
 const questionRoutes = require("./question");
+const questionFormRoutes = require("./questionsForm");
 
 // Example routes
 router.use("/example", exampleRoutes);
 router.use("/questions", questionRoutes);
+router.use("/newquestion", questionRoutes);
 
 module.exports = router;
