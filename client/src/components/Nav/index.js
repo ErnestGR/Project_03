3c87ac2fe4d074904f7bb96ad25fdc0b4b3ecfdb
Netcore89 <ernes.rave@gmail.com>
2019-03-26 19:58:34 -0600

    login navbar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Store from "../../utils/Store";
 
-function Nav() {
+function logOut() {
+  Store.set("userData", {});
+  window.location = "/";
+}
+
+function authenticatedNavBar() {
   return (
     <nav class="navbar navbar-expand-lg navbar-dark bg-light py-1">
 
@@ -27,13 +33,47 @@ function Nav() {
           Create Question
           </Link>
 
-        <Link class="btn btn-sm btn-outline-secondary" to="/newquestion">
+        <button class="btn btn-sm btn-outline-secondary" onClick={logOut}>
           Log out
+          </button>
+      </div>
+
+    </nav>
+  );
+}
+
+function unauthenticatedNavBar() {
+  return (
+    <nav class="navbar navbar-expand-lg navbar-dark bg-light py-1">
+
+      <div class="col-4 pt-1">
+        <img class="mb-4" src="img/logo_abinco.png" alt="Abinco" width="40" height="40" />
+      </div>
+
+      <div class="col-4 text-center">
+        <h2>LEAD SCORING</h2>
+      </div>
+
+      <div class="col-4 d-flex justify-content-end align-items-center">
+
+        <Link class="btn btn-sm btn-outline-secondary" to="/login">
+          Login
+          </Link>
+
+        <Link class="btn btn-sm btn-outline-secondary" to="/register">
+          Register
           </Link>
       </div>
 
     </nav>
   );
+}
+
+function Nav() {
+  const { token } = Store.get("userData");
+  return token
+    ? authenticatedNavBar()
+    : unauthenticatedNavBar();
 }
 
 export default Nav;
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -52,7 +52,6 @@ class Login extends React.Component {
     return true;
   }
 
-
   render() {
     const { email, password } = this.state;
     return (
